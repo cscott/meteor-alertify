e95f3827905b293f8d274cf2710dcd1ddee22b16
ovcharik <maksim.ovcharik@gmail.com>
2014-08-25 23:49:25 +0600

    Fix export

--- package.js
+++ package.js
@@ -8,6 +8,7 @@ Package.on_use(function (api) {
   api.add_files('vendor/lib/alertify.js', 'client');
   api.add_files('vendor/themes/alertify.core.css', 'client');
   api.add_files('vendor/themes/alertify.default.css', 'client');
+  api.add_files('main.js', 'client');
 
   if (api.export){
     api.export('alertify', 'client');
