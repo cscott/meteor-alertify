e3b66d898adace8581306850debc549c7dddbe2d
ovcharik <maksim.ovcharik@gmail.com>
2014-08-25 23:25:45 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+meteor-alertify
+===============
+
+Alertify for Meteor.
+
+
+Basic usage
+-----------
+
+http://fabien-d.github.io/alertify.js/
---
+++ package.js
@@ -0,0 +1,15 @@
+Package.describe({
+  summary: "JavaScript Alert/Notification System for Meteor. http://fabien-d.github.com/alertify.js/"
+});
+
+Package.on_use(function (api) {
+  api.use('jquery', 'client');
+
+  api.add_files('vendor/lib/alertify.js', 'client');
+  api.add_files('vendor/themes/alertify.core.css', 'client');
+  api.add_files('vendor/themes/alertify.default.css', 'client');
+
+  if (api.export){
+    api.export('alertify', 'client');
+  }
+});
